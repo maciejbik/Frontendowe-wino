06b0c4ecb040a70acbe2a6db68600477b3b456a3
Maciej Bik <maciek.bik@op.pl>
2021-08-09 13:11:47 +0200

    adding article about display grid

---
+++ main.js
@@ -0,0 +1,19 @@
+function wpisz_do_elementu(element,tekst)
+{
+    const pusty = document.querySelector(element);
+    pusty.innerHTML = tekst;
+}
+
+wpisz_do_elementu('.section__paragraph--js','Wpisywanie treści do HTML-a jak szef 😎');
+
+
+function powitanie(imie,wiek)
+{
+    console.log(`Witaj ${imie} jak tam życie mając ${wiek} lat? Jak tam pogoda? 😳`);
+    return `Witaj ${imie} jak tam życie mając ${wiek} lat? Jak tam pogoda? 😳`;
+}
+
+powitanie('Maciek',17);
+
+
+//wpisz_do_elementu('.section__paragraph--js',powitanie('Maciek',17));
